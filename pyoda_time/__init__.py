6ffc79e7203709ab61818a773313bc2e319c4dcd
Christopher McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2023-11-11 17:11:23 +0000

    Add minimal DateTimeZone implementation

--- pyoda_time/__init__.py
+++ pyoda_time/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from abc import ABC
 from datetime import datetime
 from enum import IntEnum
 from typing import Annotated, Final, Self, final, overload
@@ -221,6 +222,56 @@ class CalendarSystem:
 
     def _validate_year_month_day(self, year: int, month: int, day: int) -> None:
         self.year_month_day_calculator._validate_year_month_day(year, month, day)
+
+
+class DateTimeZone(ABC):
+    """Represents a time zone - a mapping between UTC and local time.
+    A time zone maps UTC instants to local times - or, equivalently, to the offset from UTC at any particular instant.
+    """
+
+    _UTC_ID: Final[str] = "UTC"
+
+    def __init__(self, id_: str, is_fixed: bool, min_offset: Offset, max_offset: Offset) -> None:
+        """Initializes a new instance of the DateTimeZone class.
+
+        :param id_: The unique id of this time zone.
+        :param is_fixed: Set to True is this time zone has no transitions.
+        :param min_offset: Minimum offset applied with this zone
+        :param max_offset: Maximum offset applied with this zone
+        """
+        self.__id: str = _Preconditions._check_not_null(id_, "id_")
+        self.__is_fixed: bool = is_fixed
+        self.__min_offset: Offset = min_offset
+        self.__max_offset: Offset = max_offset
+
+    @property
+    def id(self) -> str:
+        """The provider's ID for the time zone.
+
+        This identifies the time zone within the current time zone provider; a different provider may provide a
+        different time zone with the same ID, or may not provide a time zone with that ID at all.
+        """
+        return self.__id
+
+    @property
+    def _is_fixed(self) -> bool:
+        """Indicates whether the time zone is fixed, i.e. contains no transitions.
+
+        This is used as an optimization. If the time zone has no transitions but returns False for this then the
+        behavior will be correct but the system will have to do extra work. However if the time zone has transitions and
+        this returns <c>true</c> then the transitions will never be examined.
+        """
+        return self.__is_fixed
+
+    @property
+    def min_offset(self) -> Offset:
+        """The least (most negative) offset within this time zone, over all time."""
+        return self.__min_offset
+
+    @property
+    def max_offset(self) -> Offset:
+        """The greatest (most positive) offset within this time zone, over all time."""
+        return self.__max_offset
 
 
 @final
