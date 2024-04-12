c2fb6eb454bfb702238865eae51fe3da55b89daf
Chris McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2024-04-12 21:02:52 +0100

    feat: implement `LocalDatePattern` (#87)

---
+++ pyoda_time/_compatibility/_day_of_week.py
@@ -0,0 +1,17 @@
+# Copyright 2024 The Pyoda Time Authors. All rights reserved.
+# Use of this source code is governed by the Apache License 2.0,
+# as found in the LICENSE.txt file.
+
+from enum import IntEnum
+
+
+class DayOfWeek(IntEnum):
+    """Specifies the day of the week."""
+
+    Sunday = 0
+    Monday = 1
+    Tuesday = 2
+    Wednesday = 3
+    Thursday = 4
+    Friday = 5
+    Saturday = 6
