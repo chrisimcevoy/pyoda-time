b5be31dfa72ae684d700b3451b463503e04a3b75
Christopher McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2024-05-06 00:11:28 +0100

    fix: move version.py out of package

---
+++ version.py
@@ -0,0 +1,8 @@
+"""Contains the current release version.
+
+The __version__ attribute is updated automatically by release-please-action when it raises a pull request.
+The only reason this exists is so that the labeler action knows to tag those auto-generated PRs as "release".
+See also: `.github/workflows/labeler.yml`
+"""
+
+__version__ = "0.6.0"
